1e36795af53e1d2eca82b056c9aba5618e5f4753
Ma <yma2@caltech.edu>
2019-02-10 17:52:10 -0800

    Sreened for the best dropout

---
+++ NNParamScreen_YM.py
@@ -0,0 +1,36 @@
+import NeuroNetwork_YM
+import pandas as pd
+import numpy as np
+
+from keras.models import Sequential
+
+from sklearn.ensemble import VotingClassifier
+
+if __name__ == "__main__":
+    df_train = np.random.permutation(np.array(pd.read_csv("./data/train_2008.csv")))
+    X_train = df_train[0:55000, 0:-1]
+    Y_train = df_train[0:55000, -1]
+
+    X_cross = df_train[55000:, 0:-1]
+    Y_cross = df_train[55000:, -1]
+
+    df_test08 = np.array(pd.read_csv("./data/test_2008.csv"))
+    X_test08 = df_test08[:, 0:-1]
+
+    trainAcc = []
+    crossAcc = []
+    for dropRate in np.linspace(0, 0.99, 20):
+        print("rate at {}".format(dropRate))
+        nnmodel = NeuroNetwork_YM.NNmodel_YM(dpRate=dropRate)
+        nnmodel.fit(X_train, Y_train, epoch=200, verb=1)
+        trainAcc.append(nnmodel.evaluate(X_train, Y_train)[1])
+        crossAcc.append(nnmodel.evaluate(X_cross, Y_cross)[1])
+
+    # nnPredicts = nnmodel.predict(X_test08)
+
+    # submitPD = pd.DataFrame(nnPredicts, index=X_test08[:, 0], columns=['target'])
+    # submitPD.to_csv('./preDicts/nnPredicts_YM.csv', index_label='id')
+
+
+    # eclf = VotingClassifier(estimators=[('NN1', Sequential()), ('NN2', Sequential())])
+    # eclf.fit(X_train, Y_train)
--- NeuroNetwork_YM.py
+++ NeuroNetwork_YM.py
@@ -11,10 +11,12 @@ from keras.layers import Dense, Activation, Dropout
 from keras.layers import Conv2D, MaxPooling2D, Flatten, BatchNormalization
 from keras import regularizers
 
+# Entropy calculation
 def ent(labels, base=None):
     value, counts = np.unique(labels, return_counts=True)
     return entropy(counts, base=base)
 
+# return a list of filtered features
 def generate_idx50():
     df_train = np.array(pd.read_csv("./data/train_2008.csv"))
     X_train = df_train[:, 0:-1]
@@ -27,38 +29,40 @@ def generate_idx50():
 
     flags1 = entros > np.percentile(entros, 50) 
     flags2 = MIs > np.percentile(MIs, 50) 
-    flags = [a[0] & a[1] for a in zip(flags1, flags2)]
+    flags = [a[0] or a[1] for a in zip(flags1, flags2)]
     flags[0] = False
     flags[23] = False
     idx50 = np.where(flags)[0]
 
     return idx50
 
+# Reduce samample X with 
 def reduceX(X):
     X_reduced = [a for idx, a in enumerate(X.T) if idx in idx50]
     X_reduced = np.array(X_reduced).T
-
+    print(X_reduced.shape)
     return X_reduced
 
 idx50 = generate_idx50()
 
 class NNmodel_YM:
-    def __init__(self):
+    def __init__(self, dpRate=0.5):
         self.normFactor = 1
-
+        
         self.model = Sequential()
         self.model.add(Dense(128, input_shape=(len(idx50),)))
         self.model.add(BatchNormalization())
         self.model.add(Activation('relu'))
-        self.model.add(Dropout(0.5))
+        self.model.add(Dropout(dpRate))
         self.model.add(Dense(64))
         self.model.add(Activation('relu'))
         self.model.add(Dense(32))
-        self.model.add(Dropout(0.5))
+        self.model.add(Dropout(dpRate))
         self.model.add(Activation('relu'))
         self.model.add(Dense(16))
         self.model.add(Activation('relu'))
         self.model.add(Dense(4))
+        self.model.add(Dropout(dpRate))
         self.model.add(Activation('relu'))
         self.model.add(Dense(1))
         self.model.add(Activation('sigmoid'))
@@ -89,7 +93,7 @@ if __name__ == '__main__':
     Y_train = df_train[:, -1]
 
     nnmodel = NNmodel_YM()
-    nnmodel.fit(X_train, Y_train, epoch=5, verb=0)
+    nnmodel.fit(X_train, Y_train, epoch=20, verb=1)
 
     trainAcc = nnmodel.evaluate(X_train, Y_train)[1]
     print('Training Acc: {0:.3f}'.format(trainAcc))
