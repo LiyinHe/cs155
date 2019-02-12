4ff7a2889f201ed1a0cd97fc28728ec0cc96ac91
Yitong Ma <yitongma7@gmail.com>
2019-02-11 19:40:25 -0800

    added 12 predictions

--- stacking.py
+++ stacking.py
@@ -7,13 +7,16 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 if __name__ == "__main__":
+    # loading the training data
     df_train = np.random.permutation(np.array(pd.read_csv("./data/train_2008.csv")))
     X_train = df_train[:, 0:-1]
     Y_train = df_train[:, -1]
 
+    # Collection of epoches 
     epIdice = [np.arange(a, a + 13000) for a in [0, 13000, 26000, 39000]]
     epIdice.append(np.arange(52000, len(X_train)))
 
+    # Three classifiers j
     classifiers = [NeuroNetwork_YM.NNmodel_YM,
                    RandomForest_CS.RFmodel_CS,
                    xgboost_eclf7.xgbooster]
@@ -40,16 +43,26 @@ if __name__ == "__main__":
     df_test08 = np.array(pd.read_csv("./data/test_2008.csv"))
     X_test08 = df_test08
 
+    df_test12 = np.array(pd.read_csv("./data/test_2012.csv"))
+    X_test12 = df_test12
+
     feature08 = []
+    feature12 = []
     trainedModel = []
     for classifier in classifiers:
         model = classifier()
         model.fit(X_train, Y_train)
         trainedModel.append(model)
         feature08.append(model.predict(X_test08))
+        feature12.append(model.predict(X_test12))
     feature08 = np.vstack(feature08).T
+    feature12 = np.vstack(feature12).T
 
-    skPredicts = ensLog.predict_proba(feature08)[:, 1]
+    skPredicts08 = ensLog.predict_proba(feature08)[:, 1]
+    skPredicts12 = ensLog.predict_proba(feature12)[:, 1]
 
-    submitPD = pd.DataFrame(skPredicts, index=X_test08[:, 0], columns=['target'])
-    submitPD.to_csv('./predictions/skPredicts_YM.csv', index_label='id')
+    submitPD08 = pd.DataFrame(skPredicts08, index=X_test08[:, 0], columns=['target'])
+    submitPD08.to_csv('./predictions/skPredicts08.csv', index_label='id')
+
+    submitPD12 = pd.DataFrame(skPredicts12, index=X_test12[:, 0], columns=['target'])
+    submitPD12.to_csv('./predictions/skPredicts12.csv', index_label='id')
